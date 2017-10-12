87099b830e74233074f45cc3e54e6f490ed15983
Ola Lium <olalium@gmail.com>
2017-10-12 02:44:16 +0200

    implemented support for weighted graphs Task2 and almost Task3, documented code as well

--- board_manipulation.py
+++ board_manipulation.py
@@ -1,39 +1,84 @@
 
 def generate_board(input):
-    f = open('boards/board-1-'+input+'.txt', 'r')
+
+    f = open('boards/board-'+input+'.txt', 'r')
     map_array = []
+
     for line in f:
+
         map_array.append(list(line.strip()))
+
     f.close()
+
     return map_array
 
 def locate(map_array, node):
+
     y_val = -1
+
     for y in map_array:
+
         y_val += 1
         x_val = -1
+
         for x in y:
+
             x_val += 1
+
             if x is node:
+
                 return x_val, y_val
 
 def get_map_value(map_array, l):
-    y_val = -1
-    for y in map_array:
-        y_val += 1
-        x_val = -1
-        for x in y:
-            x_val += 1
-            if (x_val == l[0]) and (y_val == l[1]):
-                return map_array[y_val][x_val]
 
-def success(board, x):
+    x_val = l[0]
+    y_val = l[1]
+
+    return map_array[y_val][x_val]
+
+def success(board, x, open_d, closed):
+
     solution_board = board
+    x.state.pop()
+
+    for item in open_d.values():
+
+        solution_board[item.location[1]][item.location[0]] = 'X'
+
+    for item in closed.values():
+
+        solution_board[item.location[1]][item.location[0]] = '-'
+
     for item in x.state:
-        solution_board[item[1]][item[0]] = 'o'
+
+        solution_board[item[1]][item[0]] = '+'
+
     f = open('last_solution.txt', 'w')
+
     for line in solution_board:
+
         print(''.join(line) + '\n')
         f.write(''.join(line) + '\n')
+
     f.close()
+
     print('last_solution.tx is available in directory')
+
+def calculate_g(map_array, new_node):
+
+    map_value = get_map_value(map_array, new_node.location)
+
+    if map_value == "w":
+        value = 100
+    elif map_value == "m":
+        value = 50
+    elif map_value == "f":
+        value = 10
+    elif map_value == "g":
+        value = 5
+    elif map_value == "r":
+        value = 1
+    else:
+        value = 1
+    return value
+
--- functions.py
+++ functions.py
@@ -2,75 +2,124 @@ import board_manipulation as b_m
 from search_node import search_node
 
 def manhattan_distance(start, end):
+
     sx, sy = start
     ex, ey = end
+
     return abs(ex - sx) + abs(ey - sy)
 
 def generate_all_successors(board, node):
+
     new_nodes = {}
+    cell_categories = ["w", "m", "f", "g", "r", ".", "B"]
 
     temp_location = list(node.location)
-    temp_location[1] += 1
-    if b_m.get_map_value(board, temp_location) is '.' or b_m.get_map_value(board, temp_location) is 'B':
-        temp_location = (temp_location[0], temp_location[1])
-        new_node = search_node(G= node.g + 1, Parent=node)
-        new_node.location = temp_location
-        x = new_node.parent
-        while x != None:
-            new_node.state.append(x.location)
-            x = x.parent
-        new_nodes[temp_location] = new_node
+
+    if temp_location[1] < (len(board)-1):
+
+        temp_location[1] += 1
+
+        m_v = b_m.get_map_value(board, temp_location)
+
+        if m_v in cell_categories:
+
+            temp_location = (temp_location[0], temp_location[1])
+            new_node = search_node(G= node.g , Parent=node)
+            new_node.g += b_m.calculate_g(board, new_node)
+            new_node.location = temp_location
+            x = new_node.parent
+
+            while x != None:
+
+                new_node.state.append(x.location)
+                x = x.parent
+
+            new_nodes[temp_location] = new_node
 
     temp_location = list(node.location)
-    temp_location[0] += 1
 
-    if b_m.get_map_value(board, temp_location) is '.' or b_m.get_map_value(board, temp_location) is 'B':
-        temp_location = (temp_location[0],temp_location[1])
-        new_node = search_node(G= node.g + 1, Parent=node)
-        new_node.location = temp_location
-        x = new_node.parent
-        while x != None:
-            new_node.state.append(x.location)
-            x = x.parent
-        new_nodes[temp_location] = new_node
+    if temp_location[0] < (len(board[0])-1):
 
-    temp_location = list(node.location)
-    temp_location[1] -= 1
-    if b_m.get_map_value(board, temp_location) is '.' or b_m.get_map_value(board, temp_location) is 'B':
-        temp_location = (temp_location[0], temp_location[1])
-        new_node = search_node(G= node.g + 1, Parent=node)
-        new_node.location = temp_location
-        x = new_node.parent
-        while x != None:
-            new_node.state.append(x.location)
-            x = x.parent
-        new_nodes[temp_location] = new_node
+        temp_location[0] += 1
+
+        m_v = b_m.get_map_value(board, temp_location)
+
+        if m_v in cell_categories:
+
+            temp_location = (temp_location[0],temp_location[1])
+            new_node = search_node(G= node.g, Parent=node)
+            new_node.g += b_m.calculate_g(board, new_node)
+            new_node.location = temp_location
+            x = new_node.parent
+
+            while x != None:
+
+                new_node.state.append(x.location)
+                x = x.parent
+
+            new_nodes[temp_location] = new_node
 
     temp_location = list(node.location)
-    temp_location[0] -= 1
-    if b_m.get_map_value(board, temp_location) is '.' or b_m.get_map_value(board, temp_location) is 'B':
-        temp_location = (temp_location[0], temp_location[1])
-        new_node = search_node(G= node.g + 1, Parent=node)
-        new_node.location = temp_location
-        x = new_node.parent
-        while x != None:
-            new_node.state.append(x.location)
-            x = x.parent
-        new_nodes[temp_location] = new_node
+
+    if temp_location[1]> 0:
+
+        temp_location[1] -= 1
+
+        m_v = b_m.get_map_value(board, temp_location)
+
+        if m_v in cell_categories:
+
+            temp_location = (temp_location[0], temp_location[1])
+            new_node = search_node(G= node.g, Parent=node)
+            new_node.g += b_m.calculate_g(board, new_node)
+            new_node.location = temp_location
+            x = new_node.parent
+
+            while x != None:
+
+                new_node.state.append(x.location)
+                x = x.parent
+
+            new_nodes[temp_location] = new_node
+
+    temp_location = list(node.location)
+
+    if temp_location[0] > 0:
+
+        temp_location[0] -= 1
+
+        m_v = b_m.get_map_value(board, temp_location)
+
+        if m_v in cell_categories:
+
+            temp_location = (temp_location[0], temp_location[1])
+            new_node = search_node(G= node.g, Parent=node)
+            new_node.g += b_m.calculate_g(board, new_node)
+            new_node.location = temp_location
+            x = new_node.parent
+
+            while x != None:
+
+                new_node.state.append(x.location)
+                x = x.parent
+
+            new_nodes[temp_location] = new_node
 
     return new_nodes
 
-def attach_and_eval(child, parent, goal_location):
+def attach_and_eval(map_array, child, parent, goal_location):
+
     child.parent = parent
-    child.g = parent.g + 1
+    child.g = parent.g + b_m.calculate_g(map_array, child)
     child.h = manhattan_distance(child.location, goal_location)
     child.f = int(child.g) + int(child.h)
-    return
 
-def propagate_path_improvements(parent):
+def propagate_path_improvements(map_array, parent):
+
     for kid in parent.children:
-        if (parent.g) + 1 < kid.g:
+
+        if ((parent.g) + b_m.calculate_g(map_array, kid)) < kid.g:
+
             kid.parent = parent
-            kid.g = parent.g + 1
+            kid.g = parent.g + b_m.calculate_g(map_array, kid)
             kid.f = kid.g + kid.h
-    return
